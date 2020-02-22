55f783fe437e4741e2ebcb1ab13dc4c3bc5fdfa5
Anoop kumar <kanoop6401@gmail.com>
2020-02-22 11:29:16 +0530

    [Anoop]Added.JavaScriptProgramAccessingElementById

--- JavaScript/hello.js
+++ JavaScript/hello.js
@@ -1,1 +1,7 @@
 document.write("Welcome to JavaScrpt")
+// javaScript -- Assessing element and attribute using document object 
+
+// accessing element by document.getElementById
+var hElemnt=document.getElementById("h-element")
+alert(hElemnt)
+document.write("<br>"+hElemnt.innerHTML)
