8355eabf9057812ba5090adfb86d380a851a2f4c
kanoop640 <kanoop640@gmail.com>
2020-02-26 10:40:49 +0530

    [Anoop]Added.AccessingElementById.And.ByClass

--- JavaScript/hello.js
+++ JavaScript/hello.js
@@ -1,7 +1,23 @@
-document.write("Welcome to JavaScrpt")
-// javaScript -- Assessing element and attribute using document object 
+document.write("<br>"+"Welcome to JavaScrpt")
 
-// accessing element by document.getElementById
-var hElemnt=document.getElementById("h-element")
-alert(hElemnt)
-document.write("<br>"+hElemnt.innerHTML)
+// // JavaScript - Accessing element & attribute using document object
+
+// // accessing an element using getElementById
+// var pElement=document.getElementById("demo")
+
+// alert(pElement)
+
+// //get DOM object value
+// alert(pElement.innerHTML)
+
+// // get DOM Object attribute value
+// alert(pElement.getAttribute("id"))
+
+// // get tag Name
+// alert(pElement.tagName)
+
+// accessing element using class attribute
+
+/* var pClassElement=document.getElementsByClassName("hello") //Node list
+document.write("It is first element by accessing element by DOM property"+pClassElement[0].innerHTML+"<br>")
+document.write("It is second element by accessing element by DOM property"+pClassElement[1].innerHTML) */
